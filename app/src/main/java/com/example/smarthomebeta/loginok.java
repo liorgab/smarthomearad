012ab9dc2ec4a837b07c0ca04277e5b68e6992b0
aradg <ag7393@bs.amalnet.k12.il>
2021-01-24 13:27:05 +0200

    add constant connection arad

--- app/src/main/java/com/example/smarthomebeta/loginok.java
+++ app/src/main/java/com/example/smarthomebeta/loginok.java
@@ -1,9 +1,13 @@
 package com.example.smarthomebeta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class loginok extends AppCompatActivity {
@@ -30,4 +34,25 @@ public class loginok extends AppCompatActivity {
         startActivity(t1);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        menu.add(0,0,250,"Log Out");
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==0){
+            SharedPreferences settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+            SharedPreferences.Editor editor=settings.edit();
+            editor.putBoolean("stayConnect",false);
+            editor.commit();
+            Intent t1 = new Intent(this, MainActivity.class);
+            startActivity(t1);
+        }
+
+        return true;
+    }
+
 }
